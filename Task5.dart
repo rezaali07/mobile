2e484a0cd7815c47841bdc1b46f57723e17e3b1b
Reza Rain <rezaali20ab@gmail.com>
2024-11-19 09:02:14 +0545

    reza

---
+++ Task5.dart
@@ -0,0 +1,5 @@
+void main() {
+  // Declaring a variable to hold the user's name.
+  String name = 'Reza'; 
+  print('Hello, $name!'); // Prints a greeting message with the name.
+}
--- task1.dart
+++ task1.dart
@@ -2,16 +2,12 @@
 // Program 1
 void main() {
   int age = 18;
-  if (age > 18) {
+  if (age >= 18) {
     print('You are an adult!');
   } else {
     print('You are a minor.');
   }
 }
-
-
-
-
 
 
 
